66b518d9e8fbb6a02859a48849e44b4877249568
NavaneethaBonala <navaneethabonala@gmail.com>
2023-12-07 21:07:51 +0530

    commit

---
+++ Examples/src/examples/Mcq.java
@@ -0,0 +1,16 @@
+package examples;
+import java.util.regex.*;
+
+import java.util.*;
+
+public class Mcq{
+
+	public static void main(String s[]) {
+
+		String str = new String("java");
+
+		Object ob = str;
+
+		System.out.println(((String) ob).length());
+		}
+}
